004083228ae02bf65b3e534f6585ef221bc09fff
walid <maatallahwalid2@gmail.com>
2020-02-17 18:02:23 +0100

    add birthday selection in profile screen

--- app/src/main/java/com/travelcar/test/ui/main/account/AccountFragment.kt
+++ app/src/main/java/com/travelcar/test/ui/main/account/AccountFragment.kt
@@ -1,26 +1,30 @@
 package com.travelcar.test.ui.main.account
 
+import android.app.DatePickerDialog
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.travelcar.test.R
 import com.travelcar.test.model.User
 import com.travelcar.test.utils.DateUtils
 import kotlinx.android.synthetic.main.account_fragment.*
 import java.util.*
 
-class AccountFragment : Fragment() {
+class AccountFragment : Fragment(), DatePickerDialog.OnDateSetListener {
 
 
     private lateinit var viewModel: AccountViewModel
     private var isEditMode = false
+    private var userBirthDay = Date()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,13 +42,15 @@ class AccountFragment : Fragment() {
 
         account_button_edit_save.setOnClickListener {
             if (isEditMode) {
-                updateUiEditSaveMode(enable = false)
                 handleClickSaveUserInformation()
             } else {
                 updateUiEditSaveMode(enable = true)
+                isEditMode = isEditMode.not()
             }
+        }
 
-            isEditMode = isEditMode.not()
+        account_user_birthday.setOnClickListener {
+            showDatePickerForSelectBirthDay()
         }
     }
 
@@ -62,6 +68,7 @@ class AccountFragment : Fragment() {
         account_user_name.setText(user.firstName)
         account_last_name.setText(user.lastName)
         account_user_email.setText(user.adress)
+        userBirthDay = user.birthDate
         account_user_birthday.text = DateUtils.convertDateToString(user.birthDate)
     }
 
@@ -82,19 +89,47 @@ class AccountFragment : Fragment() {
 
     private fun handleClickSaveUserInformation() {
         if (account_user_name.text.toString().isEmpty() or account_user_email.text.toString().isEmpty() or account_user_birthday.text.toString().isEmpty()) {
-            Toast.makeText(activity, "All fields required", Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, getString(R.string.all_fields_required), Toast.LENGTH_SHORT)
+                .show()
         } else {
-            //TODO:CHANGE WITH REAL DATA
             val user = User(
                 1,
                 account_user_name.text.toString(),
                 account_last_name.text.toString(),
-                Date(),
+                userBirthDay,
                 account_user_email.text.toString()
             )
             viewModel.saveUser(user)
+            isEditMode = isEditMode.not()
+            updateUiEditSaveMode(enable = false)
+            Snackbar.make(
+                account_root_view,
+                getString(R.string.success_update_profile),
+                Snackbar.LENGTH_SHORT
+            ).show()
         }
 
+    }
+
+    private fun showDatePickerForSelectBirthDay() {
+        val calendar = GregorianCalendar().apply {
+            time = userBirthDay
+        }
+        val datePickerDialog = DatePickerDialog(
+            activity!!,
+            this,
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH) + 1,
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, dayOfMonth)
+        userBirthDay = calendar.time
+        account_user_birthday.text = DateUtils.convertDateToString(userBirthDay)
     }
 
 
